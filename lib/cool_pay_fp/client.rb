371078cd5656f06bac3744d2ac971a723d026f55
filipe paiva <filipe.p@soltroiavillas.com>
2018-12-10 04:16:35 +0000

    refractored the method call api

--- lib/cool_pay_fp/client.rb
+++ lib/cool_pay_fp/client.rb
@@ -12,35 +12,29 @@ module Coolpay
 
 
     def create_recipient(name:)
-      headers = {'Content-Type' => 'application/json',
-                 'Authorization' => "Bearer #{@token}"}
+      request = 'post'
       body = {"recipient": {"name": name}}
-      json_body = JSON.generate(body)
+      url_extension = 'recipients'
 
-      response = HTTParty.post 'https://coolpay.herokuapp.com/api/recipients', body: json_body, headers: headers
-      #Error handle
-      # create a new recipient object?
+      response = call_api request: request, body: body, url_extension: url_extension
+
     end
 
     def search_recipients(name:nil)
-      headers = {'Content-Type' => 'application/json',
-                 'Authorization' => "Bearer #{@token}"}
+      request = 'get'
+      url_extension = 'recipients'
 
       if name.nil?
-        response = HTTParty.get 'https://coolpay.herokuapp.com/api/recipients', headers: headers
-        puts JSON.parse(response.body)["recipients"]
+        response = call_api request: request, url_extension: url_extension
       else
-        response = HTTParty.get 'https://coolpay.herokuapp.com/api/recipients' + query, headers: headers
-        puts JSON.parse(response.body)["recipients"]
-
-        #error handling?
-        #adding make recipients objects?
+        query = url_extension + '?name=' + name.gsub(' ', '%20')
+        response = response = call_api request: request, url_extension: query
       end
+
+      response.code == 200 ? JSON.parse(response.body)["recipients"] : "Something is up! Check this response code: #{response.code}"
     end
 
     def make_payment(amount:, recipient_id:, currency:'GBP' )
-      headers = {'Content-Type' => 'application/json',
-                 'Authorization' => "Bearer #{@token}"}
       body = {
                 "payment": {
                   "amount": amount,
@@ -48,35 +42,48 @@ module Coolpay
                   "recipient_id": recipient_id
                 }
               }
-      json_body = JSON.generate(body)
+      request = 'post'
+      url_extension = 'payments'
 
-      response = HTTParty.post 'https://coolpay.herokuapp.com/api/payments', body: json_body, headers: headers
-      puts response.code
-
+      response = call_api request: request, url_extension: url_extension, body: body
+      response.code == 201 ? response : "Something is up! Check this response code: #{response.code}"
       #Error handle
       # create a new recipient object?
 
     end
 
     def list_payments
+      url_extension = 'payments'
+      request = 'get'
+      response = call_api request: request, url_extension: url_extension)
+      response.code == 200 ? response : "Something is up! Check this response code: #{response.code}"
+    end
+
+    private
+
+    def call_api(request:, url_extension:, body:nil)
       headers = {'Content-Type' => 'application/json',
                  'Authorization' => "Bearer #{@token}"}
-      response = HTTParty.get 'https://coolpay.herokuapp.com/api/payments', headers: headers
-      puts response.code
+
+      body.nil? ? '' : json_body = JSON.generate(body)
+
+      case request
+      when 'post'
+        HTTParty.post 'https://coolpay.herokuapp.com/api/' + url_extension, body: json_body, headers: headers
+      when 'get'
+        HTTParty.get 'https://coolpay.herokuapp.com/api/' + url_extension , headers: headers
+      end
     end
-    private
 
     def login
       headers = {'Content-Type' => 'application/json'}
       body = {username: @user_name, apikey: @api_key}
       json_body = JSON.generate(body)
 
-
       response = HTTParty.post 'https://coolpay.herokuapp.com/api/login', body: json_body, headers: headers
 
       @token = JSON.parse(response.body)["token"]
       # Error handle?
     end
-
   end
 end
