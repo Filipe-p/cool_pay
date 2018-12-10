6b0af60563073a85e5a17b5a5f93fc4ec39377c5
filipe paiva <filipe.p@soltroiavillas.com>
2018-12-10 01:06:52 +0000

    get list & searching

--- lib/cool_pay_fp/client.rb
+++ lib/cool_pay_fp/client.rb
@@ -20,6 +20,7 @@ module Coolpay
       response = HTTParty.post 'https://coolpay.herokuapp.com/api/login', body: json_body, headers: headers
 
       @token = JSON.parse(response.body)["token"]
+      # Error handle?
     end
 
     def create_recipient(name:)
@@ -27,11 +28,31 @@ module Coolpay
                  'Authorization' => "Bearer #{@token}"}
       body = {"recipient": {"name": name}}
       json_body = JSON.generate(body)
-      puts @token
 
       response = HTTParty.post 'https://coolpay.herokuapp.com/api/recipients', body: json_body, headers: headers
+      #Error handle
+      # create a new recipient object?
     end
 
+    def search_recipients(name:nil)
+      headers = {'Content-Type' => 'application/json',
+                 'Authorization' => "Bearer #{@token}"}
+
+      if name.nil?
+        puts '--------------------------------------'
+        puts 'name nil'
+        response = HTTParty.get 'https://coolpay.herokuapp.com/api/recipients', headers: headers
+        puts response.code
+        puts JSON.parse(response.body)["recipients"].count
+      else
+        query = '?name=' + name.gsub(' ', '%20')
+        puts '--------------------------------------'
+        puts "query #{query}"
+        response = HTTParty.get 'https://coolpay.herokuapp.com/api/recipients' + query, headers: headers
+        puts response.code
+        puts JSON.parse(response.body)["recipients"].count
+      end
+    end
 
   end
 end
